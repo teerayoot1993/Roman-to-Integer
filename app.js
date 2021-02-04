49ae6584fde3bf08224fc5a272b61aa2cd1dc62b
teerayoot1993 <78459035+teerayoot1993@users.noreply.github.com>
2021-02-04 10:03:58 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const values = new Map([
+  ["I", 1],
+  ["V", 5],
+  ["X", 10],
+  ["L", 50],
+  ["C", 100],
+  ["D", 500],
+  ["M", 1000],
+]);
+
+const romanToInt = (s) => {
+  let result = 0,
+    current,
+    previous = 0;
+  for (const char of s.split("").reverse()) {
+    current = values.get(char);
+    if (current >= previous) {
+      result += current;
+    } else {
+      result -= current;
+    }
+    previous = current;
+  }
+  return result;
+};
+
+console.log(romanToInt("I"));
+console.log(romanToInt("M"));
